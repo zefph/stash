bfef42250c1dc5962707631b395367b0c7241e44
zefph <zefph@users.noreply.github.com>
2019-01-09 15:40:07 +0100

    update de ses morts 2

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-/*
- * Copyright 2017 Google Inc. All rights reserved.
- *
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
- * file except in compliance with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
- * ANY KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 
 // Style credit: https://snazzymaps.com/style/1/pale-dawn
 const mapStyle = [
@@ -125,8 +111,8 @@ function initMap() {
 
   // Create the map.
   const map = new google.maps.Map(document.getElementsByClassName('map')[0], {
-    zoom: 10,
-    center: {lat: 44.839783, lng: -0.572300}, 
+    zoom: 14,
+    center: {lat: 44.84, lng: -0.590}, 
     styles: mapStyle
   });
 
@@ -163,5 +149,8 @@ function initMap() {
     infoWindow.setPosition(position);
     infoWindow.open(map);
   });
+
+  var uluru = {lat: 44.835191, lng: -0.569314};
+  var marker = new google.maps.Marker({position: uluru, map: map});
 
 }
